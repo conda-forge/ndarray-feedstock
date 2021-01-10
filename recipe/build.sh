ca83dc6e5d1f8325d789c70458c9de0171ed350f
Isuru Fernando <isuruf@gmail.com>
2021-01-10 11:59:54 -0800

    Fix deps for cross compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,13 +5,20 @@ export EIGEN_DIR=$PREFIX
 export FFTW_DIR=$PREFIX
 export CMAKE_PREFIX_PATH=$PREFIX
 
-
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} -DNDARRAY_PYBIND11=ON -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make test ARGS="-V"
+  # enable pybind11 testing only if native building
+  CMAKE_ARGS+=" -DNDARRAY_PYBIND11=ON"
+else
+  CMAKE_ARGS+=" -DNDARRAY_TEST=NO"
+fi
+
+cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+make
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+  make test ARGS="-V"
 fi
 
 make install
