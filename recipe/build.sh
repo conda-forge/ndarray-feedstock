b3a25f1ddc0c5945f734ad5dd9c72ff9acb735da
H. Vetinari <h.vetinari@gmx.com>
2023-11-24 19:26:49 +1100

    build with ninja

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 export BOOST_DIR=$PREFIX
 export EIGEN_DIR=$PREFIX
@@ -14,11 +15,12 @@ else
   CMAKE_ARGS+=" -DNDARRAY_TEST=NO"
 fi
 
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-make VERBOSE=1
+cmake -G Ninja ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+
+cmake --build . -- -v
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-  make test ARGS="-V"
+  ctest --progress --output-on-failure --verbose
 fi
 
-make install
+cmake --install .
